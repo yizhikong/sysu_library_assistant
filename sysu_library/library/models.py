50423eee3f720cd68afaf06ff3482fb0ab10db55
yizhikong <343802694@qq.com>
2015-05-17 16:07:33 +0800

    finish all API

--- sysu_library/library/models.py
+++ sysu_library/library/models.py
@@ -1,28 +1,19 @@
 from django.db import models
 
 # course in real world
+# maybe extend with jwxt
 class CourseNames(models.Model):
     cname = models.CharField(max_length = 100)
     description = models.CharField(max_length = 200)
     def __unicode__(self):
         return "Course name : " + cname
 
-# book in real world
+# book in real world, like "Computer Networking"
 class BookNames(models.Model):
     bname = models.CharField(max_length = 100)
     description = models.CharField(max_length = 200)
     def __unicode__(self):
         return "Book name : " + bname
-
-class Books(models.Model):
-    bname = models.CharField(max_length = 100)
-    isbn = models.CharField(max_length = 20)
-    author = models.CharField(max_length = 100)
-    publisher = models.CharField(max_length = 100)
-    pic = models.CharField(max_length = 100)
-    url = models.CharField(max_length = 60)
-    def __unicode__(self):
-        return "Book name : " + bname + "\nauthor : " + author + "\npublisher : " + publisher
 
 class CourseRelation(models.Model):
     # one course has several correlated books
@@ -36,3 +27,19 @@ class BookRelation(models.Model):
     book = models.ForeignKey(BookNames)
     bid = models.IntegerField(default = 0)
     click = models.IntegerField(default = 0)
+
+'''
+------------------------------------------------------------------------------------
+book(record) in sysu library
+like "Computer Networking - A Top-Down Approach", "Computer Networking Introduction"
+------------------------------------------------------------------------------------
+'''
+class Books(models.Model):
+    bname = models.CharField(max_length = 100)
+    isbn = models.CharField(max_length = 20, primary_key = True)
+    author = models.CharField(max_length = 100)
+    publisher = models.CharField(max_length = 100)
+    pic = models.CharField(max_length = 100)
+    url = models.CharField(max_length = 60)
+    def __unicode__(self):
+        return "Book name : " + bname + "\nauthor : " + author + "\npublisher : " + publisher
--- sysu_library/library/views.py
+++ sysu_library/library/views.py
@@ -6,21 +6,22 @@ from course_reptile.reptile import CourseReptile
 # from library.bookCrawler import getBooksListByName
 
 '''
+------------------------------------
 search books by couse name
 return the result as xml
 service for android application
+------------------------------------
 '''
 def searchByCourse(requset):
-    try:
-        course = requset.GET["course"]
-    except:
+    course = requset.GET.get("course", "")
+    if course == "":
         return HttpResponse("Request error") 
     try:
         # check whether this couse is in database
         c = CourseNames.objects.get(cname = course)
         # return the historic result
         xml =  getExistCourseRecord(c)
-        return xml
+        return HttpResponse(xml, mimetype="application/xml")
     except CourseNames.DoesNotExist:
         # this course has not been searched before
         # search it, and store the result in database
@@ -46,37 +47,46 @@ def searchByCourse(requset):
                 r.save()
         if xml == ""：
             return HttpResponse("No relative book for this course!")
-        head = '''<?xml version="1.0" encoding="UTF-8"?>\n''' +\
-               '''<bookList>\n'''
-        tail = '''</bookList>'''
-        return head + xml + tail
+        xml = packXml(xml, c.id, "course")
+        return HttpResponse(xml, mimetype="application/xml")
 
 # service for function searchByCourse
 def getExistCourseRecord(course_object):
-    relations = c.courserelation_set.all()
+    relations = course_object.courserelation_set.all()
     bookids = [r.bid for r in relations]
-    xml = '''<?xml version="1.0" encoding="UTF-8"?>\n''' +\
-          '''<bookList>\n'''
+    xml = ""
     for bookid in bookids:
         xml += getBookItemXml(bookid)
-    xml += '''</bookList>'''
-    return xml
+    return packXml(xml, course_object.id, "course")
 
-# create a xml for a book
+'''
+--------------------------------------------------------------------------
+the following three function search for both course search and book search
+--------------------------------------------------------------------------
+'''
+
+# add the parent node to the xml
+def packXml(xml, sid, search_type):
+    head = '''<?xml version="1.0" encoding="UTF-8"?>\n''' +\
+           '''<bookList id = "%d" type = "%s">\n'''
+    tail = '''</bookList>'''
+    resXml = (head % (sid, search_type)) + xml + tail
+    return resXml
+
+# create an xml for a book
 def getBookItemXml(bookid):
     try:
         b = Books.objects.get(id = bookid)
     except Books.DoesNotExist:
         return ""
     item_xml = '<item>\n' +\
-               '<name>%s</name>\n' +\
-               '<pic>%s</pic>\n' +\
-               '<author>%s</author>\n' +\
-               '<publisher>%s</publisher>\n' +\
-               '<num>%s</num>\n' +\
-               '<detail>%s</detail>\n' +\
+                   '<name>%s</name>\n' +\
+                   '<pic>%s</pic>\n' +\
+                   '<author>%s</author>\n' +\
+                   '<publisher>%s</publisher>\n' +\
+                   '<isbn>%s</isbn>\n' +\
                '</item>\n'
-    item_xml = item_xml % (b.name, b.pic, b.author, b.publisher, b.num, b.detail)
+    item_xml = item_xml % (b.name, b.pic, b.author, b.publisher, b.isbn)
     return item_xml
 
 # store a book item into database
@@ -94,22 +104,23 @@ def storeBookItem(item):
 
 
 '''
+----------------------------------------------------------
 search books directly by book name
 return the result as xml
 service for android application
 the logic is different from the function "searchByCourse"!
+----------------------------------------------------------
 '''
 def searchByBook(requset):
-    try:
-        course = requset.GET["book"]
-    except:
+    course = requset.GET.get("book", "")
+    if course == "":
         return HttpResponse("Request error") 
     try:
         # check whether this book is in database
         b = BookNames.objects.get(bname = course)
         # return the historic result
         xml =  getExistBookRecord(b)
-        return xml
+        return HttpResponse(xml, mimetype="application/xml")
     except BookNames.DoesNotExist:
         # this book name has not been searched before
         # search it, and store the result in database
@@ -130,20 +141,68 @@ def searchByBook(requset):
             # create the relation for this new course and the the relative book
             r = BookRelation.objects.create(book = b, bid = bookid, click = 0)
             r.save()
-        return xml
+        if xml == ""：
+            return HttpResponse("No relative records for this book!")
+        xml = packXml(xml, b.id, "book")
+        return HttpResponse(xml, mimetype="application/xml")
 
-def getExistBookRecord(b):
-    relations =  b.bookrelation_set.all()
+# maybe this function should merge with getExistCourseRecord
+# but I think it is not a good idea
+def getExistBookRecord(book_object):
+    relations = book_object.bookrelation_set.all()
     bookids = [r.bid for r in relations]
-    xml = '''<?xml version="1.0" encoding="UTF-8"?>\n''' +\
-          '''<bookList>\n'''
+    xml = ""
     for bookid in bookids:
         xml += getBookItemXml(bookid)
-    xml += '''</bookList>'''
-    return xml
+    return packXml(xml, book_object.id, "course")
 
+'''
+-----------------------------------------------------------------
+return the detail message for a book by xml
+the augument is isbn, which is the primary key for a book records
+-----------------------------------------------------------------
+'''
 def getBookDetail(requset):
-    pass
+    isbn = requset.GET.get("isbn", "")
+    if isbn == "":
+        return HttpResponse("Request error")
+    try:
+        b = Books.objects.get(isbn = isbn)
+    except BooksDoesExist:
+        return HttpResponse("ISBN error")
+    xml = '<book>\n' +\
+              '<name>%s</name>\n' +\
+              '<pic>%s</pic>\n' +\
+              '<author>%s</author>\n' +\
+              '<publisher>%s</publisher>\n' +\
+              '<isbn>%s</isbn>\n' +\
+              '<position>%s</position>\n' +\
+              '<available>%s</available>\n' +\
+              '<digest>%s</digest>\n' +\
+          '</book>\n'
+    return HttpResponse(xml, mimetype="application/xml")
 
 def clickIncrement(requset):
-    pass
+    # sid means seach id
+    sid = requset.GET.get("sid", "")
+    isbn = requset.GET.get("isbn", "")
+    search_type = requset.GET.get("search_type", "")
+    if search_type == "course":
+        try:
+            c = CourseNames.objects.get(id = sid)
+            b = Books.objects.get(isbn = isbn)
+            r = CourseRelation.objects.get(course = c, bid = b.id)
+            r.click += 1
+            r.save()
+        except:
+            return HttpResponse("Relation error")
+    if search_type == "book":
+        try:
+            bn = BookNames.objects.get(id = sid)
+            b = Books.objects.get(isbn = isbn)
+            r = BookRelation.objects.get(book = c, bid = b.id)
+            r.click += 1
+            r.save()
+        except:
+            return HttpResponse("Relation error")
+    return HttpResponse("Request error")
